d23f0ab4e2b769ccb55f79155a56e8ccb967b83f
TudorascuSorin <93769028+TudorascuSorin@users.noreply.github.com>
2022-03-05 10:11:35 +0200

    Update MAIN.java

--- TEMA_1/src/MAIN.java
+++ TEMA_1/src/MAIN.java
@@ -1,9 +1,22 @@
+import java.util.regex.*;
 
 public class MAIN {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
+		String pattern = "^Q[^u]\\d+\\.";
+        String[] input = {
+            "QA777. is the next flight. It is on time.",
+            "Quack, Quack, Quack!"
+        };
+
+        Pattern p = Pattern.compile(pattern);
+
+        for (String in : input) {
+            boolean found = p.matcher(in).lookingAt();
+
+            System.out.println(in+(found ? " -- MATCHES THE PATTERN " : " -- DOESN'T MATCH THE PATTERN "));
+        }
 	}
 
 }
